081a487772d9fa8bc3b3e09da7487d41f0bdad99
Tim Elhajj <Timothy.Elhajj@Nintex.com>
2017-04-10 15:39:02 -0700

    Initial commit

---
+++ 13.1.py
@@ -0,0 +1,24 @@
+# 13.1 xml
+
+'''
+<person>
+  <name>Chuck</name>
+  <phone type="intl">
+     +1 734 303 4456
+   </phone>
+   <email hide="yes"/>
+</person>
+'''
+
+# 13.4 json
+{
+  "name" : "Chuck",
+  "phone" : {
+    "type" : "intl",
+    "number" : "+1 734 303 4456"
+   },
+   "email" : {
+     "hide" : "yes"
+   }
+}
+
